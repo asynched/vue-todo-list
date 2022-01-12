675c9456c71be38a855aad708a13c5a4bbe18bdf
Eder Lima <devilsouls666@gmail.com>
2022-01-12 15:38:18 -0300

    feat: add page transitions

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,21 @@
 <template>
-  <router-view></router-view>
+  <transition name="fade">
+    <router-view></router-view>
+  </transition>
 </template>
+
+<style scoped>
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-to,
+.fade-leave-from {
+  opacity: 1;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all ease-in-out 250ms;
+}
+</style>
