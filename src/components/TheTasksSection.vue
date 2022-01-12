6a0e93dded3feb37122d09f2bceb785d5290351e
Eder Lima <devilsouls666@gmail.com>
2022-01-12 08:58:49 -0300

    feat: add filter for task status

--- src/components/TheTasksSection.vue
+++ src/components/TheTasksSection.vue
@@ -5,14 +5,38 @@ import TaskCard from './TaskCard.vue'
 export default {
   components: { TaskCard },
   props: ['tasks'],
+  data: () => ({
+    filter: 'All',
+  }),
+  computed: {
+    filteredTasks() {
+      if (this.filter === 'All') {
+        return this.tasks
+      }
+
+      const showOnlyFinished = this.filter === 'Finished'
+
+      return this.tasks.filter((task) => task.completed === showOnlyFinished)
+    },
+  },
 }
 </script>
 
 <template>
   <section name="tasks">
-    <p class="text-gray-500 mt-8 mb-4">TODAY'S TASKS</p>
+    <div class="mt-8 mb-4 flex items-center justify-between">
+      <p class="text-gray-500">TODAY'S TASKS</p>
+      <select
+        v-model="filter"
+        class="py-0 border-none bg-transparent focus:ring-transparent"
+      >
+        <option>All</option>
+        <option>Finished</option>
+        <option>Unfinished</option>
+      </select>
+    </div>
     <div class="grid gap-4">
-      <task-card v-for="task in tasks" :key="task.id" :task="task" />
+      <task-card v-for="task in filteredTasks" :key="task.id" :task="task" />
     </div>
   </section>
 </template>
