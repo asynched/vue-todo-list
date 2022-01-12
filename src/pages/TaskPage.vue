9270d1268728fabeedef96b39436e0c33d8e6648
Eder Lima <devilsouls666@gmail.com>
2022-01-12 15:39:25 -0300

    feat: add task page api integration

--- src/pages/TaskPage.vue
+++ src/pages/TaskPage.vue
@@ -1,9 +1,13 @@
 <script>
-import API from '@/services/http/api'
-
+import { useToast } from 'vue-toastification'
 import { TrashIcon, CheckIcon } from '@heroicons/vue/outline'
 
+import APICache from '@/lib/cache'
+import Logger from '@/utils/log'
+import API from '@/services/http/api'
+
 import BaseLayout from '@/layouts/BaseLayout.vue'
+import TaskController from '@/services/http/task-controller'
 
 export default {
   components: {
@@ -14,12 +18,62 @@ export default {
   data: () => ({
     task: null,
     isLoading: true,
+    toast: useToast(),
   }),
-  async mounted() {
-    const taskId = this.$route.params.id
-    const { data } = await API.get(`/todos/${taskId}`)
+  methods: {
+    showSuccessToast(message, toastID) {
+      if (toastID) {
+        this.toast.dismiss(toastID)
+      }
+      this.toast.success(message)
+    },
+    showErrorToast(message, toastID) {
+      if (toastID) {
+        this.toast.dismiss(toastID)
+      }
 
-    this.task = data
+      this.toast.error(message)
+    },
+    redirectToHome() {
+      this.$router.push('/')
+    },
+    async handleDelete() {
+      const taskID = this.$route.params.id
+      const cache = APICache.instance()
+
+      try {
+        await TaskController.deleteTask(taskID)
+        cache.invalidateCache()
+        this.showSuccessToast(
+          'Successfully deleted task, redirecting to home page.',
+        )
+        setTimeout(this.redirectToHome.bind(this), 3000)
+      } catch (error) {
+        Logger.error(error)
+        this.showErrorToast("Couldn't delete the task, please try again.")
+      }
+    },
+    async handleSave() {
+      const taskID = this.$route.params.id
+      const cache = APICache.instance()
+
+      const data = this.task
+
+      try {
+        await TaskController.updateTask(taskID, data)
+        cache.invalidateCache()
+        this.showSuccessToast('Succesfully updated task!')
+      } catch (error) {
+        Logger.error(error)
+        this.showErrorToast("Couldn't save the task, please try again.")
+      }
+    },
+  },
+  async mounted() {
+    const taskID = this.$route.params.id
+    const task = await TaskController.getTask(taskID)
+
+    this.task = task
     this.isLoading = false
   },
 }
@@ -32,11 +86,11 @@ export default {
     <div v-else>
       <input
         v-model="task.title"
-        class="w-full py-8 text-4xl font-bold tracking-tighter uppercase outline-none bg-transparent"
+        class="w-full mb-8 text-4xl font-bold tracking-tighter uppercase outline-none bg-transparent"
       />
       <textarea
         class="text-lg mb-4 outline-none bg-transparent w-full appearance-none border-none resize-y focus:ring-transparent"
-        value="Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugit aspernatur veniam excepturi nisi obcaecati non dolorum ullam accusantium, ut tenetur?"
+        v-model="task.description"
       />
       <div class="mb-4 flex items-center gap-2 text-lg">
         <input type="checkbox" v-model="task.completed" class="rounded" />
@@ -46,12 +100,14 @@ export default {
         <button
           class="w-12 h-12 grid place-items-center border-2 text-red-600 border-red-600 rounded transition ease-in-out hover:bg-red-600 hover:text-white hover:shadow-md hover:shadow-red-300"
           aria-label="Delete task"
+          @click="handleDelete"
         >
           <trash-icon class="w-5 h-5" />
         </button>
         <button
           class="w-12 h-12 grid place-items-center rounded transition ease-in-out bg-blue-600 text-white hover:shadow-md hover:shadow-blue-300"
           aria-label="Save task"
+          @click="handleSave"
         >
           <check-icon class="w-5 h-5" />
         </button>
