673e926f6f0d7fb5e26053a80b7de969e40aad51
Eder Lima <devilsouls666@gmail.com>
2022-01-12 15:41:25 -0300

    feat: add animations for components

--- src/components/TheCategoriesOverviewSection.vue
+++ src/components/TheCategoriesOverviewSection.vue
@@ -22,20 +22,45 @@ export default {
   <section name="categories-overview">
     <p class="text-gray-500 mb-4">CATEGORIES</p>
     <div class="grid grid-cols-2 gap-4">
-      <category-overview-card
-        background="bg-blue-600"
-        shadow="shadow-blue-400"
-        name="Completed"
-        :tasks-count="completed"
-        :total="total"
-      />
-      <category-overview-card
-        background="bg-red-600"
-        shadow="shadow-red-400"
-        name="To be finished"
-        :tasks-count="notCompleted"
-        :total="total"
-      />
+      <transition name="bounce" appear>
+        <category-overview-card
+          background="bg-blue-600"
+          shadow="shadow-blue-400"
+          name="Completed"
+          :tasks-count="completed"
+          :total="total"
+        />
+      </transition>
+      <transition name="bounce" appear>
+        <category-overview-card
+          background="bg-red-600"
+          shadow="shadow-red-400"
+          name="To be finished"
+          :tasks-count="notCompleted"
+          :total="total"
+        />
+      </transition>
     </div>
   </section>
 </template>
+
+<style scoped>
+.bounce-enter-active {
+  animation: bounce-enter 500ms ease-in;
+}
+
+@keyframes bounce-enter {
+  0% {
+    transform: translate(0, 10%);
+    opacity: 0;
+  }
+  50% {
+    transform: translate(0, -5%);
+    opacity: 50%;
+  }
+  100% {
+    transform: translate(0, 0);
+    opacity: 1;
+  }
+}
+</style>
--- src/components/TheTasksSection.vue
+++ src/components/TheTasksSection.vue
@@ -35,8 +35,27 @@ export default {
         <option>Unfinished</option>
       </select>
     </div>
-    <div class="grid gap-4">
+    <transition-group appear name="tasks" tag="div" class="grid gap-4">
       <task-card v-for="task in filteredTasks" :key="task.id" :task="task" />
-    </div>
+    </transition-group>
   </section>
 </template>
+
+<style scoped>
+.tasks-enter-active {
+  animation: show-up 500ms ease-in-out;
+}
+
+.tasks-move {
+  transition: transform 500ms;
+}
+
+@keyframes show-up {
+  0% {
+  }
+  100% {
+    transform: translate(0, 0);
+    opacity: 1;
+  }
+}
+</style>
